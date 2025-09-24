6cc878788cc47a67b52b42c113113ca8054bf510
Ashim Maskey <ashimmaskey4@gmail.com>
2025-09-24 12:21:52 +0545

    intialized shadcn(1806)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
-  return <div className="text-red-300">Home</div>;
+  return (
+    <div className="text-red-300">
+      <Button variant={"destructive"}>Click me</Button>
+    </div>
+  );
 };
 
 export default Home;
