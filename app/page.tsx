7b2cf9215e103bb8f305c8d44278be4b0cace42c
Ashim Maskey <ashimmaskey4@gmail.com>
2025-10-09 14:35:48 +0545

    user table created

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,30 @@
-import { Button } from "@/components/ui/button";
+// "use client";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
 
 const Home = () => {
+  // const user = useUser();
+  // console.log(user);
   return (
-    <div className="text-red-300">
-      <Button variant={"destructive"}>Click me</Button>
-    </div>
+    <>
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton>
+          <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+            Sign Up
+          </button>
+        </SignUpButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </>
   );
 };
 
