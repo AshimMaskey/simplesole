d3c4027bc3a47bc339b73bc133c0c28184313eb3
Ashim Maskey <ashimmaskey4@gmail.com>
2025-11-18 12:03:02 +0545

    added cache testing

--- app/(users)/shop/actions/productActions.ts
+++ app/(users)/shop/actions/productActions.ts
@@ -1,12 +1,12 @@
 "use server";
 
-// import { prisma } from "@/lib/prisma";
 import prisma from "@/lib/prisma";
+import { cache } from "react";
 
-export const getProducts = async () => {
+export const getProducts = cache(async () => {
   const products = await prisma.product.findMany({
     include: { variants: true },
     orderBy: { created_at: "desc" },
   });
   return products;
-};
+});
